b8cfc49bdcfe7f03594050fd3a334b8f41cf9b56
Alex Reshta <alex.reshta@gmail.com>
2018-03-30 22:05:09 +0300

    crypto next dev step

--- psw-store/cr-try.py
+++ psw-store/cr-try.py
@@ -1,15 +1,20 @@
 from Crypto.Cipher import AES
 from Crypto import Random
 
-key = b'Sixteen byte key'
+skey = "some_key"*3
+print (skey[0:16])
+key = str.encode(skey[0:16])
+textForCoding = """This is some super text for coding.
+Excelent solution!"""
 iv = key
+
 #iv = Random.new().read(AES.block_size)
-cipher = AES.new(key, AES.MODE_CFB, iv)
-msg = cipher.encrypt(b'Attack at dawn')
+cipher = AES.new(key, AES.MODE_CFB, key)
+msg = cipher.encrypt(str.encode(textForCoding))
 
 
 
 print(msg)
-cipher = AES.new(key, AES.MODE_CFB, iv)
-dec = cipher.decrypt(msg)
+cipher = AES.new(key, AES.MODE_CFB, key)
+dec = cipher.decrypt(msg).decode()
 print (dec)
--- psw-store/my-cryp-note.py
+++ psw-store/my-cryp-note.py
@@ -1,35 +1,81 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from tkinter import Tk, Text, Scrollbar, Menu, messagebox, filedialog, BooleanVar, Checkbutton, Label, Entry, StringVar, Grid, Frame
+from tkinter import Tk, Text, Scrollbar, Menu, messagebox, filedialog, BooleanVar, Checkbutton, Label, Entry, StringVar, Grid, Frame, Button, Toplevel
 import os, subprocess, json, string
 from datetime import datetime
+from Crypto.Cipher import AES
 
 import areditor
 
 class ArFile:
-    fpath = "./in.txt2"
+    def __init__(self, fpath):
+        self.fpath = fpath
 
     def check(self):
         return os.path.exists(self.fpath)
 
-class ArCryptor:
-    def __init__(self):
-        a = 1
+class ArCrypt:
+    def __init__(self, psw):
+        #print( psw)
+        self.skey = psw * 3
+        print(self.skey[0:16])
+        self.key = str.encode(self.skey[0:16])
+
+    def encrypt(self,txt):
+        self.cipher = AES.new(self.key, AES.MODE_CFB, self.key)
+        self.msg = self.cipher.encrypt(str.encode(txt))
+        return self.msg
+
+
+class PswDialog:
+
+    def __init__(self, parent):
+
+        top = self.top = Toplevel(parent)
+
+        Label(top, text="Password: ").pack()
+
+        self.e = Entry(top, show = '*')
+        self.e.pack(padx=5)
+
+        b = Button(top, text="OK", command=self.ok)
+        b.pack(pady=5)
+
+    def ok(self):
+
+        #print ("value is", self.e.get())
+        self.psw = self.e.get();
+
+        self.top.destroy()
 
 if __name__ == "__main__":
-    arFile = ArFile()
+
+    arFile = ArFile("./my1.bin")
+
     root = Tk()
+    d = PswDialog(root)
+    root.wait_window(d.top)
+
+    crypt = ArCrypt(d.psw)
+
     root.wm_state('normal')
+
     editor = areditor.Editor(root)
     editor.file_path = arFile.fpath
     editor.set_title()
     editor.main()
 
+    #Button(root, text="Hello!").pack()
+    #root.update()
+
     if not arFile.check():
         dt = datetime.now()
-        with open(ArFile.fpath, 'a') as the_file:
-            the_file.write('Initial:' + str(datetime.now()) + '\n')
+        with open(arFile.fpath, 'ab') as the_file:
+            msg = 'Initial:' + str(datetime.now()) + '\n';
+            crMsg = crypt.encrypt(msg)
+            #print(crMsg)
+            the_file.write(bytearray(crMsg))
 
-    editor.file_open(filepath=ArFile.fpath)
+    editor.file_open(filepath=arFile.fpath)
     root.mainloop()
