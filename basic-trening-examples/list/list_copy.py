50c2ec054ad3d337d438984b2b34fcf39bde9a0b
Alexey Reshta <areshta@luxoft.com>
2018-10-17 09:08:44 +0300

    deep copy

--- basic-trening-examples/list/list_copy.py
+++ basic-trening-examples/list/list_copy.py
@@ -7,9 +7,10 @@ list1 = []
 list2 = [1,2,3,4]
 list1 = list2
 list2[1] = -1
-print(list1) #it is not copy! it is alias
+print("list1 = list2 is not copy!\nlist2 = ", list2)
+print("list1 = ", list1)
 
-print("\n*** Shallow copy copyes one level only! ***\n")
+print("\n*** Shallow copy copies one level only! ***\n")
 
 list1 = []
 list2 = [1,[-1,-2,-3],2,3]
@@ -17,14 +18,20 @@ list1 = list2.copy()
 list2.append(4)
 list2[0] = 0;
 
-print("looks as endipendent copy. list1 = ", list1)
+print("looks as independent copy. \nlist2 = ", list2)
+print("list1 = ", list1)
 
 list2[1][1] = -10
-print("Ops! It was not recursive! References are shared! list1 = ", list1)
+print("\nOps! It was not recursive! References are shared!\nlist2 = ", list2)
+print("list1 = ", list1)
 
 print("\n*** Real deep copy ***\n")
 list1 = copy.deepcopy(list2)
 list2[1][1] = -100
-print("Copy.deepcopy is real depp copy ", list1)
+list2[0]=-10
+list2.append(-200)
+print("copy.deepcopy is real deep copy.\nlist2 = ", list2)
+print("list1 = ", list1)
+
 
 
