294685dccf4579c8b3ba08cb306cce8bdebe91f8
Alex Reshta <alex.reshta@gmail.com>
2018-10-20 21:38:59 +0300

    dictionary example

---
+++ basic-trening-examples/dict/dict-1.py
@@ -0,0 +1,19 @@
+# Python-3 dictionary example
+# For example, we registrate some colored objects
+# and whant to know how many objects of every color was.
+
+# was registrated:
+colors = ["red","blue","red", "cyan", "black", "blue", "red", 
+          "red", "orange", "black", "cyan", "red", "pink",
+          "orange", "black", "cyan", "red", "pink", "blue",
+          "red", "green", "blue", "red", "green", "blue", 
+          "blue","red", "cyan", "black", "blue", "green"]
+
+dc = {} # we will use dictionary of color:counter pares
+for cl in colors:
+    if dc.get(cl) == None:
+        dc[cl] = 1
+    else:
+        dc[cl] = dc[cl]+1
+
+print(dc) 
