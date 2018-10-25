bbcac8a4019d29700111cf65f06825b3cc81fce0
Alex Reshta <alex.reshta@gmail.com>
2018-10-25 20:27:48 +0300

    minor fix

--- basic-trening-examples/flow/bool.py
+++ basic-trening-examples/flow/bool.py
@@ -2,15 +2,25 @@
 arg1 = (False, True)
 arg2 = (False, True)
 
-print("Truth table: \n")
+print("\n*** Truth table ***\n")
 print("{:-^30}".format(""))
 print("! arg1 ! arg2 !!  and !  or  !")
 print("{:-^30}".format(""))
-
 format_str = "!{:^6}!{:^6}!!{:^6}!{:^6}!\n"
+
 str1 = format_str.format(arg1[0],arg2[0],arg1[0] and arg2[0], arg1[0] or arg2[0]) 
 str2 = format_str.format(arg1[0],arg2[1],arg1[0] and arg2[1], arg1[0] or arg2[1]) 
 str3 = format_str.format(arg1[1],arg2[0],arg1[1] and arg2[0], arg1[1] or arg2[0]) 
 str4 = format_str.format(arg1[1],arg2[1],arg1[1] and arg2[1], arg1[1] or arg2[1]) 
+
 print(str1 + str2 +str3 + str4)
+
+print("{:-^18}".format(""))
+print("! arg1 !! not  !")
+print("{:-^18}".format(""))
+format_str = "!{:^6}!!!{:^6}!\n"
+str1 = format_str.format(arg1[0],not arg1[0])
+str2 = format_str.format(arg1[1],not arg1[1]) 
+print(str1 + str2) 
+
 
