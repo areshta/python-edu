88c2d5785c5efd0fd961332262ad869497c99c6a
Alex Reshta <alex.reshta@gmail.com>
2018-10-27 20:06:13 +0300

    improving factorial demo

--- basic-training-examples/functions/factorial.py
+++ basic-training-examples/functions/factorial.py
@@ -1,12 +1,29 @@
-#Python-3. recursion example
+#Python-3. Functions example. Recursion and itteration. 
+#          Can be used as module. 
+#          Help can be call in Python interactive mode
 
-print("\n*** Factorial calculation ***\n")
-
-def factorial(n):
+def factorial_rc(n):
+    """recursive solution"""
     if n<1:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial_rc(n-1)
 
-for i in range(1,11):
-    print("{:2}! = ".format(i), factorial(i))
+def factorial_it(n):
+    """itterative solution"""
+    mp = 1
+    for i in range(2, n+1):
+        mp *= i
+    return mp
+
+if __name__ == "__main__":
+
+    print("\n*** Factorial calculation. Recursive and Itteration ***\n")
+
+    for i in range(1,11):
+        print("{:2}! = recursive {:8}   and   itterative {:8}"
+                .format(i, factorial_rc(i), factorial_it(i)) )
+
+    print("\nUsing docstring inside programm:")
+    print(factorial_rc.__doc__)
+    print(factorial_it.__doc__)
