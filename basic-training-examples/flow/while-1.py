2b0ef12db5e80ca2bda7c4887b89f1165ecc6a01
Alexey Reshta <areshta@luxoft.com>
2018-10-27 11:48:31 +0300

    for and while

---
+++ basic-training-examples/flow/while-1.py
@@ -0,0 +1,12 @@
+#Python-3 "wile" example
+
+print("\n*** Test your patience ***\n")
+
+level = 0
+answer = 'Y'
+
+while answer.upper() != 'N':
+	print(f"Your patience level = {level}.  Continue?[n - no]")
+	level += 1
+	answer = input()
+
